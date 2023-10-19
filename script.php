2e0a92a8fba1be541869c9a31a2ef21278a3b508
jerwin149115 <115704976+jerwin149115@users.noreply.github.com>
2023-10-20 01:20:51 +0800

    Update script.php

--- script.php
+++ script.php
@@ -1,9 +1,22 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = $_POST['data']; // Retrieve data sent from Java
-    // Process the data (e.g., perform actions based on the data)
-    echo "Processed data: " . $data;
-} else {
-    echo "Invalid request";
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Queue Management System</h1>
+        <div class="queue-number">
+            <h2>Current Queue Number:</h2>
+            <?php
+            if (isset($_POST['numberQueue'])) {
+                $numberQueue = $_POST['numberQueue'];
+                echo '<p>' . $numberQueue . '</p>';
+            } else {
+                echo '<p>No data received</p>';
+            }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
