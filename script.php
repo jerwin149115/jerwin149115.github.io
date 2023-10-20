88f3ad1cada719390a41da0e63fd3076156dc78b
jerwin149115 <115704976+jerwin149115@users.noreply.github.com>
2023-10-20 14:04:54 +0800

    Update script.php

--- script.php
+++ script.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Queue Management System</h1>
-        <div class="queue-number">
-            <h2>Current Queue Number:</h2>
-            <?php
-            if (isset($_POST['numberQueue'])) {
-                $numberQueue = $_POST['numberQueue'];
-                echo '<p>' . $numberQueue . '</p>';
-            } else {
-                echo '<p>No data received</p>';
-            }
-            ?>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = $_POST['data']; 
+    echo "Processed data: " . $data;
+} else {
+    echo "Invalid request";
+}
+?>
