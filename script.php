461d794f6474ac1e599e61bb63b02c1be3e77838
jerwin149115 <115704976+jerwin149115@users.noreply.github.com>
2023-11-05 15:33:05 +0800

    Update and rename script.php to script.js

--- script.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST' ] == 'POST') {
-    $data = $_POST['postData'] ?? '';
-    $response = ['message' => 'Data received sucessfully','data' => $data];
-    echo json_encode($response);
-} else {
-    echo 'no data received';
-}
-?>
